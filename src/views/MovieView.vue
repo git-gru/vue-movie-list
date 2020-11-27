2cc3393d99898801eaf28efc71892742c9d06a5f
Felonius Gru <new-comet-dev@outlook.com>
2020-11-27 12:03:12 +0900

    update movie view interface

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,21 +1,53 @@
 <template>
-  <div class="movie-view">
-    <h1>This is the movie view</h1>
+  <div v-if="Object.keys(movieData).length" class="movie-view">
+    <h1>{{ movieData.Title }}</h1>
+
+    <img :src="movieData.Poster" />
+    <div class="movie-detail">
+      <div>Year: {{ movieData.Year }}</div>
+      <div>Type: {{ movieData.Type }}</div>
+      <div>Rated: {{ movieData.Rated }}</div>
+      <div>Released: {{ movieData.Released }}</div>
+      <div>Production: {{ movieData.Production }}</div>
+      <div>Plot: {{ movieData.Plot }}</div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import MovieService from '@/services/MovieService';
+import { MovieDetail } from '../services/types';
 
 export default Vue.extend({
   name: 'MovieView',
+
+  data() {
+    return {
+      movieData: {} as MovieDetail,
+    };
+  },
+
   async created() {
-    MovieService.movieService.getSpecificMovie(/* TODO */ '273b9080', /* TODO */ 'tt0372784');
+    await this.loadMovie();
+  },
+
+  methods: {
+    async loadMovie() {
+      this.movieData = await MovieService.movieService.getSpecificMovie(
+        process.env.VUE_APP_OMDB_API_KEY,
+        this.$route.params.movieId,
+      );
+    },
   },
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.movie-view {
+  .movie-detail {
+    text-align: center;
+  }
+}
 </style>
